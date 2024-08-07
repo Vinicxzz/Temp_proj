55a1f0ae36780c94932a493d828d2efa780b74cb
Vinicius Silva <viniciusti0102@.gmail.com>
2024-08-07 13:25:07 +0100

    PrevProps and PrevState test

--- src/TextInput/index.jsx
+++ src/TextInput/index.jsx
@@ -1,3 +1,4 @@
+import './styles.css';
 export const TextInput = ({ searchValue, handleChange }) => {
   return (
     <input
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,70 +1,30 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { Component } from 'react';
 import './styles.css';
-import { loadPosts } from "../../utils/load-posts";
-import { Posts } from '../../components/Posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../TextInput';
 
-export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(10); // Removed setter as it's not being changed
-  const [searchValue, setSearchValue] = useState("");
+export class Home extends Component {
+  state = {
+    counter: 0
+  }
 
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-  const filteredPosts = searchValue
-    ? allPosts.filter(post =>
-      post.title.toLowerCase().includes(searchValue.toLowerCase())
+  handleClick = () => {
+    const { counter } = this.state;
+    this.setState((prevState, prevProps) => {
+
+      return { counter: prevState.counter + prevProps.numberToIncrement }
+
+    }, () => {
+      console.log('POST', this.state.counter)
+    }
     )
-    : posts;
 
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
+  }
 
-    setPosts(postsAndPhotos.slice(0, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
-
-  useEffect(() => {
-    handleLoadPosts(page, postsPerPage);
-  }, [handleLoadPosts, page, postsPerPage]);
-
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    setPosts((prevPosts) => [...prevPosts, ...nextPosts]);
-    setPage(nextPage);
-  };
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
-
-  return (
-    <section className='container'>
-      <div className="search-container">
-        {!!searchValue && (
-          <h1>Search value: {searchValue}</h1>
-        )}
-        <TextInput searchValue={searchValue} handleChange={handleChange} />
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleClick}>Increment</button>
       </div>
-      {filteredPosts.length > 0 ? (
-        <Posts posts={filteredPosts} />
-      ) : (
-        <p>Não existem Posts</p>
-      )}
-
-      <div className='button-container'>
-        {!searchValue && (
-          <Button
-            text="Load more Posts"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
-      </div>
-    </section>
-  );
-};
+    );
+  }
+}
